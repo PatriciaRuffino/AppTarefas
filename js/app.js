6589e256ddae9297a0ebd74af673e22a3dd0841e
PatriciaRuffino <sindelformr@gmail.com>
2021-09-07 13:17:19 -0300

    referenciando elementos

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//refenrenciar o input
+let input= document.querySelector('input[name=tarefa]')
+//referenciar o button
+let btn = document.querySelector('#botao')
+//referenciar a lista
+let lista = document.querySelector('#lista')
+//console.log(lista)
+let tarefas =[
+    "estudar python", "estudar JavaScript", "estudar java", "assistir séries","fazer jump"
+]
+
+{/* <li class="list-group-item list-group-item-action">Estudar Java</li>
+<li class="list-group-item list-group-item-action">Estudar JavaScript</li>
+<li class="list-group-item list-group-item-action">Estudar Python</li>
+<li class="list-group-item list-group-item-action">Estudar React</li>
+<li class="list-group-item list-group-item-action">Estudar Infra</li> */}
